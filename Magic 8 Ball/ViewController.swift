5d2a2dc24a0914b07c63e4b60a4f38c3467799bf
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-18 17:59:05 +0400

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Guga Gongadze on 11/18/18.
+//  Copyright © 2018 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
